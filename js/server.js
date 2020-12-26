fc010f198ed1a7314ba4577f4db83c7aef4e5a08
sergio roma <sergioroma@sergios-MBP.lan>
2020-12-26 22:02:48 +0000

    Views File Created and basic setup to the server.js file.

--- js/server.js
+++ js/server.js
@@ -6,6 +6,21 @@ const server = require('http').Server(app);
 // Bring in socket io and pass the express server. 
 const io = require('socket.io')(server);
 
+const {v4: uuidV4} = require('uuid');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>{
+    res.redirect(`/${uuidV4()}`);
+});
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room });
+});
+
+
+
 // Server will run on port 3000
 server.listen(3000);
-
+console.log("Server Running in Port :: 3000");
